f21daefe4fe9884ebff494956731a3b4452dbdbb
Unknown <emrecancakiroglu1@gmail.com>
2019-01-16 22:12:07 +0300

    Alpha 1.2
    Endpoints added

---
+++ Public/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * NoviaNet Cloud - Cloud Services
+ *
+ * @package  NoviaNet Cloud
+ * @author   NoviaNet Developers <dev@novianet.net>
+ */
+
+ /**
+  * ---------------------------------------------------------------------
+  *  Get framework loaded
+  * ---------------------------------------------------------------------
+  */
+
+  require("../load.php");
+  
+  /**
+   * --------------------------------------------------------------------
+   *  THAT'S IT! Isn't that amazing that everything works only by
+   *  including a single file. I think not.
+   * --------------------------------------------------------------------
+   */
---
+++ Route/web.php
@@ -0,0 +1,14 @@
+<?php
+use Bootstrap\Route;
+/**
+ * ------------------------------------------------
+ * Routing system
+ * ------------------------------------------------
+ * 
+ * Examples:
+ * Route::get("/", Controller@Method);
+ * Route::redirect("from", "to");
+ * Route::error("404", function() {
+ *  // Something...
+ * });
+ */
