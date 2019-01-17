0fe03f4a32da4074101814a05a62a60cd4578d32
Unknown <emrecancakiroglu1@gmail.com>
2019-01-18 00:52:33 +0300

    Comments and Middlewares added
    Comments and Middlewares are added. Now load file can load Middlewares.

--- load.php
+++ load.php
@@ -1,11 +1,39 @@
 <?php
-
+/**
+ * Session.
+ */
 require_once("../bootstrap/Session.class.php");
 
 Bootstrap\Session::start();
 
+/**
+ * Database and functionality.
+ */
 require_once("../bootstrap/Database.class.php");
+
+/**
+ * Load models. (This is not doing anything yet.)
+ */
 require_once("../bootstrap/Model.class.php");
+
+/**
+ * Load Middlewares.
+ */
+require_once("../bootstrap/Middleware.class.php");
+
+Bootstrap\Middleware::loadAll();
+
+/**
+ * Loading Routing system.
+ */
 require_once("../bootstrap/Route.class.php");
+
+/**
+ * Loading Controller system.
+ */
 require_once("../bootstrap/Controller.class.php");
+
+/**
+ * Loading the application itsellf by including web.
+ */
 require_once("../Route/web.php");
